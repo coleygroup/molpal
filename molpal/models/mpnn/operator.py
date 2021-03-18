5f3a3c91c229c24c412e3f5e0deb577e840dee3f
david graff <deg711@g.harvard.edu>
2021-03-18 18:20:27 -0400

    fixing lack of optimizer.zero_grad() in training_batch()

--- molpal/models/mpnn/operator.py
+++ molpal/models/mpnn/operator.py
@@ -1,16 +1,14 @@
-from argparse import Namespace
-from typing import Dict, Iterable, List, Optional, Sequence, Tuple, TypeVar
+from typing import Dict, Iterable, List, Sequence, Tuple, TypeVar
 
-import numpy as np
 from ray.util.sgd.torch import TrainingOperator
 import torch
-from torch import nn, cuda
+from torch.nn import functional as F
+from torch.optim import Adam
 
-from ..chemprop.data.data import (MoleculeDatapoint, MoleculeDataset,
-                                 MoleculeDataLoader)
+from ..chemprop.data.data import MoleculeDatapoint, MoleculeDataset
 from ..chemprop.data.scaler import StandardScaler
 from ..chemprop.data.utils import split_data
-from .. import chemprop
+from ..chemprop.nn_utils import NoamLR
 
 from molpal.models import mpnn
 
@@ -19,40 +17,60 @@ T_feat = TypeVar('T_feat')
 
 class MPNNOperator(TrainingOperator):
     def setup(self, config):
-        smis = config['smis']
-        targets = config['targets']
+        # smis = config['smis']
+        # targets = config['targets']
 
-        self.scaler = None
-        train_data, val_data = self.make_datasets(smis, targets)
-        train_data_size = len(train_data) + len(val_data)
+        # self.scaler = None
+        # train_data, val_data = self.make_datasets(smis, targets)
+        # train_data_size = len(train_data) + len(val_data)
 
         # scheduler_args = config['scheduler_args']
         # self.scheduler_args = scheduler_args
         
-        train_loader = MoleculeDataLoader(
-            dataset=train_data,
-            batch_size=config['batch_size'],
-            num_workers=config.get('ncpu', 1)
-        )
-        val_loader = MoleculeDataLoader(
-            dataset=val_data,
-            batch_size=config['batch_size'],
-            num_workers=config.get('ncpu', 1)
-        )
+        # train_loader = MoleculeDataLoader(
+        #     dataset=train_data,
+        #     batch_size=config['batch_size'],
+        #     num_workers=config.get('ncpu', 1)
+        # )
+        # val_loader = MoleculeDataLoader(
+        #     dataset=val_data,
+        #     batch_size=config['batch_size'],
+        #     num_workers=config.get('ncpu', 1)
+        # )
+
+        warmup_epochs = config.get('warmup_epochs', 2.)
+        steps_per_epoch = config['steps_per_epoch']
+        max_epochs = config['max_epochs']
+        num_lrs = 1
+        init_lr = config.get('init_lr', 1e-4)
+        max_lr = config.get('max_lr', 1e-3)
+        final_lr = config.get('final_lr', 1e-4)
+        uncertainty_method = config.get('uncertainty_method', 'none')
 
         model = config['model']
-        optimizer = chemprop.utils.build_optimizer(
-            model, config['init_lr']
+        optimizer = Adam([{
+            'params': model.parameters(), 'lr': init_lr, 'weight_decay': 0
+        }])
+        # optimizer = Adam(model.parameters(), init_lr, weight_decay=0)
+        scheduler = NoamLR(
+            optimizer=optimizer, warmup_epochs=[warmup_epochs],
+            total_epochs=[max_epochs] * num_lrs,
+            steps_per_epoch=steps_per_epoch,
+            init_lr=[init_lr], max_lr=[max_lr], final_lr=[final_lr]
         )
-        scheduler = chemprop.utils.build_lr_scheduler(
-            optimizer, train_data_size=train_data_size,
-            **config['scheduler_args'])
         criterion = mpnn.utils.get_loss_func(
-            config['dataset_type'], config['uncertainty_method']
+            config['dataset_type'], uncertainty_method
         )
 
-        self.uncertainty = config['uncertainty_method'] in {'mve'}
-        self.metric_func = chemprop.utils.get_metric_func(config['metric'])
+        self.uncertainty = uncertainty_method in {'mve'}
+
+        self.metric = {
+            'mse': lambda X, Y: F.mse_loss(X, Y, reduction='none'),
+            'rmse': lambda X, Y: torch.sqrt(F.mse_loss(X, Y, reduction='none'))
+        }[config.get('metric', 'rmse')]
+
+        train_loader = config['train_loader']
+        val_loader = config['val_loader']
 
         self.n_iter = 0
         self.best_val_score = float('-inf')
@@ -67,101 +85,138 @@ class MPNNOperator(TrainingOperator):
             train_loader=train_loader, validation_loader=val_loader
         )
     
-    def train_epoch(self, train_loader, info):
-        #train_loader = [x for x in train_loader]
-        #print(train_loader)
-        self.n_iter += mpnn.train(
-            self.model, train_loader, self.criterion,
-            self.optimizer, self.scheduler, self.uncertainty, self.n_iter, True
-        )
-        return {}
+    # def train_epoch(self, train_loader, info):
+    #     #train_loader = [x for x in train_loader]
+    #     #print(train_loader)
+    #     self.n_iter += mpnn.train(
+    #         self.model, train_loader, self.criterion,
+    #         self.optimizer, self.scheduler, self.uncertainty, self.n_iter, True
+    #     )
+    #     return {}
     
     def train_batch(self, batch: MoleculeDataset, batch_info: Dict) -> Dict:
-        mol_batch = batch.batch_graph()
-        
+        componentss, targets = batch
+
         model = self.model
         optimizer = self.optimizer
         criterion = self.criterion
-
-        preds = model(mol_batch)
-        targets = batch.targets()
+        scheduler = self.scheduler
         
+        optimizer.zero_grad()
+
+        # look @ "non_blocking=True" if weird things start happening
+        device = 'cuda' if self.use_gpu else 'cpu'
+        componentss = [[
+            X.to(device, non_blocking=True)
+                if isinstance(X, torch.Tensor) else X
+            for X in components
+        ] for components in componentss]
+
         mask = torch.tensor(
-            [list(map(bool, ys)) for ys in targets], device=self.device
+            [[bool(y) for y in ys] for ys in targets], device=device
         )
         targets = torch.tensor(
-            [[y or 0 for y in ys] for ys in targets], device=self.device
+            [[y or 0 for y in ys] for ys in targets], device=device
         )
-        class_weights = torch.ones(targets.shape, device=self.device)
-        
-        # if args.dataset_type == 'multiclass':
+        class_weights = torch.ones(targets.shape, device=device)
+
+        preds = model(componentss)
+        # if self.dataset_type == 'multiclass':
         #     targets = targets.long()
         #     loss = (torch.cat([
-        #         loss_func(preds[:, target_index, :],
-        #                    targets[:, target_index]).unsqueeze(1)
+        #         criterion(
+        #             preds[:, target_index, :], targets[:, target_index]
+        #         ).unsqueeze(1)
         #         for target_index in range(preds.size(1))
-        #         ], dim=1) * class_weights * mask
-        #     )
+        #     ], dim=1) * class_weights * mask)
 
         if self.uncertainty:
             pred_means = preds[:, 0::2]
             pred_vars = preds[:, 1::2]
 
-            loss = self.criterion(pred_means, pred_vars, targets)
+            loss = criterion(pred_means, pred_vars, targets)
         else:
-            loss = self.criterion(preds, targets) * class_weights * mask
+            loss = criterion(preds, targets) * class_weights * mask
 
         loss = loss.sum() / mask.sum()
-        return loss
+
+        loss.backward()
+        optimizer.step()
+        if isinstance(scheduler, NoamLR):
+            scheduler.step()
+
+        return {'train_loss': loss.item(), 'num_samples': len(targets)}
     
-    def validate(self, val_loader, info):
-        val_loader = [item for item in val_loader]
-        #print(val_loader)
-        val_scores = mpnn.evaluate(
-            self.model, val_loader, 1, self.uncertainty,
-            self.metric_func, 'regession', self.scaler)
-        val_score = np.nanmean(val_scores)
+    # def validate(self, val_loader, info):
+    #     val_loader = [item for item in val_loader]
+    #     #print(val_loader)
+    #     val_scores = mpnn.evaluate(
+    #         self.model, val_loader, 1, self.uncertainty,
+    #         self.metric_func, 'regession', self.scaler)
+    #     val_score = np.nanmean(val_scores)
 
-        if val_score > self.best_val_score:
-            self.best_val_score = val_score
-            self.best_state_dict = self.model.state_dict()
+    #     if val_score > self.best_val_score:
+    #         self.best_val_score = val_score
+    #         self.best_state_dict = self.model.state_dict()
 
-        return {}
+    #     return {}
     
-    def validation_step(self, batch: MoleculeDataset, batch_idx) -> List[float]:
-        preds = self.mpnn(batch.batch_graph())
+    def validate(self, val_iterator, info):
+        """Runs one standard validation pass over the val_iterator.
+        
+        Args:
+            val_iterator (iter): Iterable constructed from the
+                validation dataloader.
+            info: (dict): Dictionary for information to be used for custom
+                validation operations.
+        Returns:
+            A dict of metrics from the evaluation.
+                By default, returns "val_accuracy" and "val_loss"
+                which is computed by aggregating "loss" and "correct" values
+                from ``validate_batch`` and dividing it by the sum of
+                ``num_samples`` from all calls to ``self.validate_batch``.
+        """
+        self.model.eval()
 
+        losses, num_samples = [], 0
+        with torch.no_grad():
+            for batch_idx, batch in enumerate(val_iterator):
+                batch_info = {"batch_idx": batch_idx}
+                batch_info.update(info)
+                step_results = self.validate_batch(batch, batch_info)
+                losses.append(step_results['loss'])
+                num_samples += step_results['num_samples']
+
+            val_loss = torch.cat(losses).mean().item()
+
+        return {
+            'val_loss': val_loss, #/ num_samples,
+            'num_samples': num_samples
+        }
+
+    def validate_batch(self, batch: MoleculeDataset, batch_idx) -> List[float]:
+        componentss, targets = batch
+
+        model = self.model
+        metric = self.metric
+
+        device = 'cuda' if self.use_gpu else 'cpu'
+        componentss = [[
+            X.to(device, non_blocking=True)
+                if isinstance(X, torch.Tensor) else X
+            for X in components
+        ] for components in componentss]
+        targets = torch.tensor(targets, device=device)
+
+        preds = model(componentss)
         if self.uncertainty:
             preds = preds[:, 0::2]
 
-        # preds_ = preds.cpu().numpy()
-        targets_ = batch.targets()
-        targets = torch.tensor(targets_, device=self.device)
-
-        res = torch.sqrt(nn.MSELoss()(preds, targets))
-        return res
+        loss = metric(preds, targets)
+        return {'loss': loss, 'num_samples': len(targets)}
     
-    def get_model(self):
-        model = super().get_model()
-        model.load_state_dict(self.best_state_dict)
+    # def get_model(self):
+    #     model = super().get_model()
+    #     model.load_state_dict(self.best_state_dict)
 
-        return model
-
-    def make_datasets(
-            self, xs: Iterable[str], ys: Sequence[float]
-        ) -> Tuple[MoleculeDataset, MoleculeDataset]:
-        """Split xs and ys into train and validation datasets"""
-
-        data = MoleculeDataset([
-            MoleculeDatapoint(smiles=[x], targets=[y])
-            for x, y in zip(xs, ys)
-        ])
-        train_data, val_data, _ = split_data(data=data, sizes=(0.8, 0.2, 0.0))
-
-        train_targets = train_data.targets()
-        self.scaler = StandardScaler().fit(train_targets)
-
-        scaled_targets = self.scaler.transform(train_targets).tolist()
-        train_data.set_targets(scaled_targets)
-
-        return train_data, val_data
+    #     return model
