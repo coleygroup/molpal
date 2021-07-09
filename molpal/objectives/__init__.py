5115f6ea2e2f621c1b6cbf2090f10ab1fc88b328
david graff <deg711@g.harvard.edu>
2021-07-09 17:45:40 -0400

    changing objectives to use objective config files only rather than command line args; need to fix docking objective later

--- molpal/objectives/__init__.py
+++ molpal/objectives/__init__.py
@@ -2,13 +2,13 @@ from typing import Type
 
 from molpal.objectives.base import Objective
 
-def objective(objective, **kwargs) -> Type[Objective]:
+def objective(objective, objective_config: str, **kwargs) -> Type[Objective]:
     """Objective factory function"""
     if objective == 'docking':
         from molpal.objectives.docking import DockingObjective
-        return DockingObjective(**kwargs)
+        return DockingObjective(objective_config)
     if objective == 'lookup':
         from molpal.objectives.lookup import LookupObjective
-        return LookupObjective(**kwargs)
+        return LookupObjective(objective_config)
     
     raise NotImplementedError(f'Unrecognized objective: "{objective}"')
