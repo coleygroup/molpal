1172bb8b7f9e9cd9dab26f88427c5372a58f2227
david graff <deg711@g.harvard.edu>
2021-08-03 16:00:21 -0400

    removing a bunch of old commented code

--- molpal/objectives/__init__.py
+++ molpal/objectives/__init__.py
@@ -6,9 +6,9 @@ def objective(objective, objective_config: str, **kwargs) -> Type[Objective]:
     """Objective factory function"""
     if objective == 'docking':
         from molpal.objectives.docking import DockingObjective
-        return DockingObjective(objective_config)
+        return DockingObjective(objective_config, **kwargs)
     if objective == 'lookup':
         from molpal.objectives.lookup import LookupObjective
-        return LookupObjective(objective_config)
+        return LookupObjective(objective_config, **kwargs)
     
     raise NotImplementedError(f'Unrecognized objective: "{objective}"')
