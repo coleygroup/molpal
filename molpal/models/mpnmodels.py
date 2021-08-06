0a9a71566c9251aabd913a76f6ad62605869fb7d
david graff <deg711@g.harvard.edu>
2021-08-06 11:01:16 -0400

    cleaning up code

--- molpal/models/mpnmodels.py
+++ molpal/models/mpnmodels.py
@@ -19,7 +19,6 @@ from .chemprop.data.data import (
 )
 from .chemprop.data.scaler import StandardScaler
 from .chemprop.data.utils import split_data
-from . import chemprop
 
 from molpal.models.base import Model
 from molpal.models import mpnn, utils
