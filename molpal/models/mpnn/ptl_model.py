1717cd4a12ef85d3669043a798d826aefc77fb9c
david graff <deg711@g.harvard.edu>
2021-10-07 18:04:42 -0400

    style changes

--- molpal/models/mpnn/ptl_model.py
+++ molpal/models/mpnn/ptl_model.py
@@ -9,36 +9,36 @@ from torch.nn import functional as F
 from molpal.models import mpnn
 from ..chemprop.nn_utils import NoamLR
 
-logging.getLogger('lightning').setLevel(logging.FATAL)
+logging.getLogger("lightning").setLevel(logging.FATAL)
+
 
 class LitMPNN(pl.LightningModule):
     """A message-passing neural network base class"""
+
     def __init__(self, config: Optional[Dict] = None):
         super().__init__()
         config = config or {}
 
-        self.mpnn = config.get('model', mpnn.MoleculeModel())
-        self.uncertainty = config.get('uncertainty', 'none')
-        self.dataset_type = config.get('dataset_type', 'regression')
+        self.mpnn = config.get("model", mpnn.MoleculeModel())
+        self.uncertainty = config.get("uncertainty", "none")
+        self.dataset_type = config.get("dataset_type", "regression")
 
-        self.warmup_epochs = config.get('warmup_epochs', 2.)
-        self.max_epochs = config.get('max_epochs', 50)
+        self.warmup_epochs = config.get("warmup_epochs", 2.0)
+        self.max_epochs = config.get("max_epochs", 50)
         self.num_lrs = 1
-        self.init_lr = config.get('init_lr', 1e-4)
-        self.max_lr = config.get('max_lr', 1e-3)
-        self.final_lr = config.get('final_lr', 1e-4)
+        self.init_lr = config.get("init_lr", 1e-4)
+        self.max_lr = config.get("max_lr", 1e-3)
+        self.final_lr = config.get("final_lr", 1e-4)
 
-        self.criterion = mpnn.utils.get_loss_func(
-            self.dataset_type, self.uncertainty
-        )
+        self.criterion = mpnn.utils.get_loss_func(self.dataset_type, self.uncertainty)
         self.metric = {
-            'mse': lambda X, Y: F.mse_loss(X, Y, reduction='none'),
-            'rmse': lambda X, Y: torch.sqrt(F.mse_loss(X, Y, reduction='none'))
-        }.get(config.get('metric', 'rmse'), 'rmse')
+            "mse": lambda X, Y: F.mse_loss(X, Y, reduction="none"),
+            "rmse": lambda X, Y: torch.sqrt(F.mse_loss(X, Y, reduction="none")),
+        }.get(config.get("metric", "rmse"), "rmse")
 
     def training_step(self, batch: Tuple, batch_idx) -> torch.Tensor:
         componentss, targets = batch
-        
+
         preds = self.mpnn(componentss)
         mask = torch.tensor(
             [[bool(y) for y in ys] for ys in targets], device=self.device
@@ -47,7 +47,7 @@ class LitMPNN(pl.LightningModule):
             [[y or 0 for y in ys] for ys in targets], device=self.device
         )
         class_weights = torch.ones(targets.shape, device=self.device)
-        
+
         # if args.dataset_type == 'multiclass':
         #     targets = targets.long()
         #     loss = (torch.cat([
@@ -57,7 +57,7 @@ class LitMPNN(pl.LightningModule):
         #         ], dim=1) * class_weights * mask
         #     )
 
-        if self.uncertainty == 'mve':
+        if self.uncertainty == "mve":
             pred_means = preds[:, 0::2]
             pred_vars = preds[:, 1::2]
 
@@ -67,17 +67,17 @@ class LitMPNN(pl.LightningModule):
 
         loss = loss.sum() / mask.sum()
         return loss
-    
+
     def training_epoch_end(self, outputs):
-        losses = [d['loss'] for d in outputs]
+        losses = [d["loss"] for d in outputs]
         train_loss = torch.stack(losses, dim=0).mean()
-        self.log('train_loss', train_loss)
+        self.log("train_loss", train_loss)
 
     def validation_step(self, batch: Tuple, batch_idx) -> List[float]:
         componentss, targets = batch
-        
+
         preds = self.mpnn(componentss)
-        if self.uncertainty == 'mve':
+        if self.uncertainty == "mve":
             preds = preds[:, 0::2]
 
         targets = torch.tensor(targets, device=self.device)
@@ -86,14 +86,12 @@ class LitMPNN(pl.LightningModule):
 
     def validation_epoch_end(self, outputs):
         val_loss = torch.cat(outputs).mean()
-        self.log('val_loss', val_loss)
+        self.log("val_loss", val_loss)
 
     def configure_optimizers(self) -> List:
-        opt = Adam([{
-            'params': self.mpnn.parameters(),
-            'lr': self.init_lr,
-            'weight_decay': 0
-        }])
+        opt = Adam(
+            [{"params": self.mpnn.parameters(), "lr": self.init_lr, "weight_decay": 0}]
+        )
         sched = NoamLR(
             optimizer=opt,
             warmup_epochs=[self.warmup_epochs],
@@ -101,13 +99,13 @@ class LitMPNN(pl.LightningModule):
             steps_per_epoch=self.num_training_steps,
             init_lr=[self.init_lr],
             max_lr=[self.max_lr],
-            final_lr=[self.final_lr]
+            final_lr=[self.final_lr],
         )
         scheduler = {
-            'scheduler': sched,
-            'interval': 'step' if isinstance(sched, NoamLR) else 'batch'
+            "scheduler": sched,
+            "interval": "step" if isinstance(sched, NoamLR) else "batch",
         }
-        
+
         return [opt], [scheduler]
 
     @property
@@ -122,7 +120,7 @@ class LitMPNN(pl.LightningModule):
         if isinstance(limit_batches, int):
             batches = min(batches, limit_batches)
         else:
-            batches = int(limit_batches * batches)     
+            batches = int(limit_batches * batches)
 
         num_devices = max(1, self.trainer.num_gpus, self.trainer.num_processes)
         if self.trainer.tpu_cores:
--- molpal/models/mpnn/utils.py
+++ molpal/models/mpnn/utils.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
-from torch import clamp, log, nn
+from torch import clamp, nn
 
-def get_loss_func(dataset_type: str,
-                  uncertainty_method: Optional[str] = None) -> nn.Module:
+
+def get_loss_func(
+    dataset_type: str, uncertainty_method: Optional[str] = None
+) -> nn.Module:
     """Get the loss function corresponding to a given dataset type
 
     Parameters
@@ -23,23 +25,20 @@ def get_loss_func(dataset_type: str,
     ValueError
         if is dataset_type is neither "classification" nor "regression"
     """
-    if dataset_type == 'classification':
-        return nn.BCEWithLogitsLoss(reduction='none')
+    if dataset_type == "classification":
+        return nn.BCEWithLogitsLoss(reduction="none")
 
-    elif dataset_type == 'regression':
-        if uncertainty_method == 'mve':
+    elif dataset_type == "regression":
+        if uncertainty_method == "mve":
             return negative_log_likelihood
 
-        return nn.MSELoss(reduction='none')
+        return nn.MSELoss(reduction="none")
 
     raise ValueError(f'Unsupported dataset type: "{dataset_type}."')
 
-def negative_log_likelihood(pred_mean, pred_var, targets):
+
+def negative_log_likelihood(means, variances, targets):
     """The NLL loss function as defined in:
     Nix, D.; Weigend, A. ICNN’94. 1994; pp 55–60 vol.1"""
-    clamped_var = clamp(pred_var, min=0.00001)
-    return (
-        log(clamped_var)/2
-        + (pred_mean - targets)**2 / (2*clamped_var)
-    )
-
+    variances = clamp(variances, min=1e-5)
+    return (variances.log() + (means - targets) ** 2 / variances) / 2
