a0eb19a8793a8f3e6fe58116a13f0aedfea5e53f
david graff <deg711@g.harvard.edu>
2021-10-15 16:18:56 -0400

    add `setup.py` and move packages to `pip` in `environment.yml`

---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+molpal
+active learning to accelerate virtual drug discovery
+"""
+import sys
+from setuptools import setup, find_packages
+import versioneer
+
+short_description = __doc__.split("\n")
+
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except:
+    long_description = "\n".join(short_description[2:])
+
+
+setup(
+    name="molpal",
+    author="david graff",
+    author_email="deg711@g.harvard.edu",
+    description=short_description[0],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    setup_requires=[],
+    url="https://github.com/coleygroup/molpal",
+    platforms=["Linux", "Mac OS-X", "Unix"],
+    python_requires=">=3.7",
+    install_requires=[
+        "configargparse",
+        "h5py",
+        "numpy",
+        "ray[default]",
+        "scikit_learn",
+        "tensorflow",
+        "tensorflow_addons",
+        "tqdm",
+    ],
+)
