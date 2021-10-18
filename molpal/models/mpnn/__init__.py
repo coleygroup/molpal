d15c87dd03aca2f802c5dc036d3f5bb44e1b536b
david graff <deg711@g.harvard.edu>
2021-10-18 14:20:59 -0400

    reorganize mpnn folder

--- molpal/models/mpnn/__init__.py
+++ molpal/models/mpnn/__init__.py
@@ -1,8 +1,6 @@
-from .callbacks import EpochAndStepProgressBar
 from .evaluate import evaluate
 from .model import MoleculeModel
-from .operator import MPNNOperator
 from .predict import predict
-from .ptl_model import LitMPNN
+from .ptl import EpochAndStepProgressBar, LitMPNN
 from .train import train
 from . import sgd, utils
