b10bede3a597fcdcaf04b755ca35fbff0f3b0fa0
david graff <deg711@g.harvard.edu>
2021-10-21 13:30:14 -0400

    add acquirer tests and change style

--- molpal/acquirer/acquirer.py
+++ molpal/acquirer/acquirer.py
@@ -4,15 +4,15 @@ import heapq
 from itertools import chain
 import math
 from timeit import default_timer
-from typing import (Callable, Dict, Iterable, List, Mapping, 
-                    Optional, Set, TypeVar, Union)
+from typing import Dict, Iterable, List, Mapping, Optional, Set, TypeVar, Union
 
 import numpy as np
 from tqdm import tqdm
 
 from molpal.acquirer import metrics
 
-T = TypeVar('T')
+T = TypeVar("T")
+
 
 class Acquirer:
     """An Acquirer acquires inputs from an input pool for exploration.
@@ -39,7 +39,7 @@ class Acquirer:
         the threshold value to use in the random_threshold metric
     verbose : int
         the level of output the acquirer should print
-    
+
     Parameters
     ----------
     size : int
@@ -62,58 +62,66 @@ class Acquirer:
     **kwargs
         additional and unused keyword arguments
     """
-    def __init__(self, size: int,
-                 init_size: Union[int, float] = 0.01,
-                 batch_sizes: Iterable[Union[int, float]] = [0.01],
-                 metric: str = 'greedy',
-                 epsilon: float = 0., beta: int = 2, xi: float = 0.01,
-                 threshold: float = float('-inf'),
-                 temp_i: Optional[float] = None, temp_f: Optional[float] = 1.,
-                 seed: Optional[int] = None, verbose: int = 0, **kwargs):
+
+    def __init__(
+        self,
+        size: int,
+        init_size: Union[int, float] = 0.01,
+        batch_sizes: Iterable[Union[int, float]] = [0.01],
+        metric: str = "greedy",
+        epsilon: float = 0.0,
+        beta: int = 2,
+        xi: float = 0.01,
+        threshold: float = float("-inf"),
+        temp_i: Optional[float] = None,
+        temp_f: Optional[float] = 1.0,
+        seed: Optional[int] = None,
+        verbose: int = 0,
+        **kwargs,
+    ):
         self.size = size
         self.init_size = init_size
         self.batch_sizes = batch_sizes
-        # self.batch_size = next(self.batch_sizes)
 
         self.metric = metric
         self.stochastic_preds = False
 
-        if not 0. <= epsilon <= 1.:
-            raise ValueError(f'Epsilon(={epsilon}) must be in [0, 1]')
+        if not 0.0 <= epsilon <= 1.0:
+            raise ValueError(f"Epsilon(={epsilon}) must be in [0, 1]")
         self.epsilon = epsilon
-        
+
         self.beta = beta
         self.xi = xi
         self.threshold = threshold
 
         self.temp_i = temp_i
         self.temp_f = temp_f
-
-        metrics.set_seed(seed)
+        self.seed = seed
         self.verbose = verbose
+
+        metrics.set_seed(self.seed)
 
     def __len__(self) -> int:
         return self.size
 
     @property
     def needs(self) -> Set[str]:
-        """Set[str] : the values this acquirer needs to calculate acquisition 
-        utilities"""
+        """the set of values this acquirer needs to calculate acquisition utilities"""
         return metrics.get_needs(self.metric)
-    
+
     @property
     def init_size(self) -> int:
         """the number of inputs to acquire initially"""
         return self.__init_size
-    
+
     @init_size.setter
     def init_size(self, init_size: Union[int, float]):
         if isinstance(init_size, float):
             if init_size < 0 or init_size > 1:
-                raise ValueError(f'init_size(={init_size} must be in [0, 1]')
+                raise ValueError(f"init_size(={init_size} must be in [0, 1]")
             init_size = math.ceil(self.size * init_size)
         if init_size < 0:
-            raise ValueError(f'init_size(={init_size}) must be positive')
+            raise ValueError(f"init_size(={init_size}) must be positive")
 
         self.__init_size = init_size
 
@@ -121,7 +129,7 @@ class Acquirer:
     def batch_sizes(self) -> List[int]:
         """the number of inputs to acquire in exploration batch"""
         return self.__batch_sizes
-    
+
     @batch_sizes.setter
     def batch_sizes(self, batch_sizes: Iterable[Union[int, float]]):
         self.__batch_sizes = [bs for bs in batch_sizes]
@@ -129,15 +137,21 @@ class Acquirer:
         for i, bs in enumerate(self.__batch_sizes):
             if isinstance(bs, float):
                 if bs < 0 or bs > 1:
-                    raise ValueError(f'batch_size(={bs} must be in [0, 1]')
+                    raise ValueError(f"batch_size(={bs} must be in [0, 1]")
                 self.__batch_sizes[i] = math.ceil(self.size * bs)
             if bs < 0:
-                raise ValueError(f'batch_size(={bs} must be positive')
+                raise ValueError(f"batch_size(={bs} must be positive")
 
-    def acquire_initial(self, xs: Iterable[T],
-                        cluster_ids: Optional[Iterable[int]] = None,
-                        cluster_sizes: Optional[Mapping[int, int]] = None,
-                        ) -> List[T]:
+    def reset(self):
+        """reset the random state of the metrics module"""
+        metrics.set_seed(self.seed)
+
+    def acquire_initial(
+        self,
+        xs: Iterable[T],
+        cluster_ids: Optional[Iterable[int]] = None,
+        cluster_sizes: Optional[Mapping[int, int]] = None,
+    ) -> List[T]:
         """Acquire an initial set of inputs to explore
 
         Parameters
@@ -160,19 +174,20 @@ class Acquirer:
 
         if cluster_ids is None and cluster_sizes is None:
             heap = []
-            for x, u in tqdm(zip(xs, U), total=U.size, desc='Acquiring'):
+            for x, u in tqdm(zip(xs, U), total=U.size, desc="Acquiring"):
                 if len(heap) < self.init_size:
                     heapq.heappush(heap, (u, x))
                 else:
                     heapq.heappushpop(heap, (u, x))
         else:
             d_cid_heap = {
-                cid: ([], math.ceil(self.init_size * cluster_size/U.size))
+                cid: ([], math.ceil(self.init_size * cluster_size / U.size))
                 for cid, cluster_size in cluster_sizes.items()
             }
 
-            for x, u, cid in tqdm(zip(xs, U, cluster_ids), total=U.size,
-                                  desc='Acquiring'):
+            for x, u, cid in tqdm(
+                zip(xs, U, cluster_ids), "Acquiring", U.size, disable=self.verbose < 1
+            ):
                 heap, heap_size = d_cid_heap[cid]
                 if len(heap) < heap_size:
                     heapq.heappush(heap, (u, x))
@@ -183,16 +198,22 @@ class Acquirer:
             heap = list(chain(*heaps))
 
         if self.verbose > 0:
-            print(f'  Selected {len(heap)} initial samples')
+            print(f"  Selected {len(heap)} initial samples")
 
         return [x for _, x in heap]
 
-    def acquire_batch(self, xs: Iterable[T],
-                      y_means: Iterable[float], y_vars: Iterable[float],
-                      explored: Optional[Mapping] = None, k: int = 1,
-                      cluster_ids: Optional[Iterable[int]] = None,
-                      cluster_sizes: Optional[Mapping[int, int]] = None,
-                      t: Optional[int] = None, **kwargs) -> List[T]:
+    def acquire_batch(
+        self,
+        xs: Iterable[T],
+        y_means: Iterable[float],
+        y_vars: Iterable[float],
+        explored: Optional[Mapping] = None,
+        k: int = 1,
+        cluster_ids: Optional[Iterable[int]] = None,
+        cluster_sizes: Optional[Mapping[int, int]] = None,
+        t: Optional[int] = None,
+        **kwargs,
+    ) -> List[T]:
         """Acquire a batch of inputs to explore
 
         Parameters
@@ -226,17 +247,13 @@ class Acquirer:
             ys = list(explored.values())
             Y = np.nan_to_num(np.array(ys, dtype=float), nan=-np.inf)
             current_max = np.partition(Y, -k)[-k]
-            # try:
-            #     current_max = max(y for y in explored.values() if y is not None)
-            # except ValueError:
-            #     current_max = float('-inf')
         else:
             explored = {}
-            current_max = float('-inf')
+            current_max = float("-inf")
 
         try:
             batch_size = self.batch_sizes[t]
-        except IndexError:
+        except (IndexError, TypeError):
             batch_size = self.batch_sizes[-1]
 
         begin = default_timer()
@@ -245,30 +262,32 @@ class Acquirer:
         Y_var = np.array(y_vars)
 
         if self.verbose > 1:
-            print('Calculating acquisition utilities ...', end=' ')
+            print("Calculating acquisition utilities ...", end=" ")
 
         U = metrics.calc(
-            self.metric, Y_mean=Y_mean, Y_var=Y_var, current_max=current_max, 
-            t=self.threshold, beta=self.beta, xi=self.xi,
-            stochastic=self.stochastic_preds
+            self.metric,
+            Y_mean,
+            Y_var,
+            current_max,
+            self.threshold,
+            self.beta,
+            self.xi,
+            self.stochastic_preds
         )
 
-        idxs = np.random.choice(
-            np.arange(U.size), replace=False,
-            size=int(batch_size * self.epsilon)
-        )
+        idxs = np.random.choice(U.size, math.ceil(batch_size * self.epsilon), False)
         U[idxs] = np.inf
 
         if self.verbose > 1:
-            print('Done!')
+            print("Done!")
         if self.verbose > 2:
             total = default_timer() - begin
             mins, secs = divmod(int(total), 60)
-            print(f'      Utility calculation took {mins}m {secs}s')
-        
+            print(f"      Utility calculation took {mins}m {secs}s")
+
         if cluster_ids is None and cluster_sizes is None:
             heap = []
-            for x, u in tqdm(zip(xs, U), total=U.size, desc='Acquiring'):
+            for x, u in tqdm(zip(xs, U), "Acquiring", U.size, disable=self.verbose < 1):
                 if x in explored:
                     continue
 
@@ -281,14 +300,15 @@ class Acquirer:
             # the random indices are not distributed evenly amongst clusters
 
             d_cid_heap = {
-                cid: ([], math.ceil(batch_size * cluster_size/U.size))
+                cid: ([], math.ceil(batch_size * cluster_size / U.size))
                 for cid, cluster_size in cluster_sizes.items()
             }
 
-            global_pred_max = float('-inf')
+            global_pred_max = float("-inf")
 
-            for x, y_pred, u, cid in tqdm(zip(xs, Y_mean, U, cluster_ids),
-                                          total=U.size, desc='Acquiring'):
+            for x, y_pred, u, cid in tqdm(
+                zip(xs, Y_mean, U, cluster_ids), total=U.size, desc="Acquiring"
+            ):
                 global_pred_max = max(y_pred, global_pred_max)
 
                 if x in explored:
@@ -301,32 +321,27 @@ class Acquirer:
                     heapq.heappushpop(heap, (u, x))
 
             if self.temp_i and self.temp_f:
-                d_cid_heap = self._scale_heaps(
-                    d_cid_heap, global_pred_max, t,
-                    self.temp_i, self.temp_f
-                )
+                d_cid_heap = self.scale_heaps(d_cid_heap, global_pred_max, t)
 
             heaps = [heap for heap, _ in d_cid_heap.values()]
             heap = list(chain(*heaps))
 
         if self.verbose > 1:
-            print(f'Selected {len(heap)} new samples')
+            print(f"Selected {len(heap)} new samples")
         if self.verbose > 2:
             total = default_timer() - begin
             mins, secs = divmod(int(total), 60)
-            print(f'      Batch acquisition took {mins}m {secs}s')
+            print(f"      Batch acquisition took {mins}m {secs}s")
 
         return [x for _, x in heap]
 
-    def _scale_heaps(self, d_cid_heap: Dict[int, List],
-                     pred_global_max: float, t: int,
-                     temp_i: float, temp_f: float):
+    def scale_heaps(self, d_cid_heap: Dict[int, List], global_pred_max: float, it: int):
         """Scale each heap's size based on a decay factor
 
         The decay factor is calculated by an exponential decay based on the
         difference between a given heap's local maximum and the predicted
         global maximum then scaled by the current temperature. The temperature
-        is also an exponential decay based on the current iteration starting at 
+        is also an exponential decay based on the current iteration starting at
         the initial temperature and approaching the final temperature.
 
         Parameters
@@ -336,7 +351,7 @@ class Acquirer:
             acquire from that cluster
         pred_global_max : float
             the predicted maximum value of the objective function
-        t : int
+        it : int
             the current iteration of acquisition
         temp_i : float
             the initial temperature of the system
@@ -348,7 +363,7 @@ class Acquirer:
         d_cid_heap
             the original mapping scaled down by the calculated decay factor
         """
-        temp = self._calc_temp(t, temp_i, temp_f)
+        temp = Acquirer.temp(it, self.temp_i, self.temp_f)
 
         for cid, (heap, heap_size) in d_cid_heap.items():
             if len(heap) == 0:
@@ -357,22 +372,21 @@ class Acquirer:
             pred_local_max = max(
                 heap, key=lambda yx: -1 if math.isinf(yx[0]) else yx[0]
             )
-            f = self._calc_decay(pred_global_max, pred_local_max, temp)
+            lam = Acquirer.decay(global_pred_max, pred_local_max, temp)
 
-            new_heap_size = math.ceil(f * heap_size)
+            new_heap_size = math.ceil(lam * heap_size)
             new_heap = heapq.nlargest(new_heap_size, heap)
 
             d_cid_heap[cid] = (new_heap, new_heap_size)
 
         return d_cid_heap
 
-    @classmethod
-    def _calc_temp(cls, t: int, temp_i, temp_f) -> float:
+    @staticmethod
+    def temp(it: int, temp_i, temp_f) -> float:
         """Calculate the temperature of the system"""
-        return temp_i * math.exp(-t/0.75) + temp_f
+        return (temp_i - temp_f) * math.exp(-it) + temp_f
 
-    @classmethod
-    def _calc_decay(cls, global_max: float, local_max: float,
-                    temp: float) -> float:
-        """Calculate the decay factor of a given heap"""
-        return math.exp(-(global_max - local_max)/temp)
+    @staticmethod
+    def decay(global_max: float, local_max: float, temp: float) -> float:
+        """Calculate the decay factor lambda of a given heap"""
+        return math.exp(-(global_max - local_max) / temp)
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,116 +1,98 @@
-import unittest
-
 import numpy as np
+import pytest
 
 from molpal.acquirer import metrics
 
-class TestMetrics(unittest.TestCase):    
-    def test_random(self):
-        Y_mean = np.arange(10)
+@pytest.fixture(params=[0, 1, 2, 4])
+def beta(request):
+    return request.param
 
-        U = metrics.random(Y_mean)
-        self.assertEqual(U.shape, Y_mean.shape)
+@pytest.fixture(params=[-1, 0, 1000])
+def curr_max(request):
+    return request.param
 
-    def test_greedy(self):
-        Y_mean = np.arange(10)
+@pytest.fixture(params=[-1, 0, 5, 1000])
+def threshold(request):
+    return request.param
 
-        U = metrics.greedy(Y_mean)
-        np.testing.assert_array_equal(U, Y_mean)
+@pytest.fixture(params=[0., 0.01, 0.1])
+def xi(request):
+    return request.param
 
-    def test_noisy(self):
-        Y_mean = np.arange(10)
-        sd = np.std(Y_mean)
+@pytest.fixture(params=[True, False])
+def stochastic(request):
+    return request.param
 
-        U = metrics.noisy(Y_mean)
-        np.testing.assert_allclose(U, Y_mean, atol=4*sd)
+@pytest.fixture(params=[10, 50, 100])
+def size(request):
+    return request.param
 
-    def test_ucb_no_var(self):
-        Y_mean = np.arange(10)
-        Y_var = np.zeros(Y_mean.shape)
-        beta = 2
+@pytest.fixture
+def Y_mean(size: int):
+    return size*np.random.normal(size=size) #np.arange(size)
+
+@pytest.fixture
+def Y_var_0(size: int):
+    return np.zeros(size)
+
+@pytest.fixture
+def Y_var(size: int):
+    return np.random.rand(size) / 10
+
+def test_random(Y_mean):
+    U = metrics.random(Y_mean)
+
+    np.testing.assert_equal(Y_mean.shape, U.shape)
+
+def test_greedy(Y_mean):
+    U = metrics.greedy(Y_mean)
+
+    np.testing.assert_array_equal(Y_mean, U)
+
+def test_noisy(Y_mean):
+    sd = np.std(Y_mean)
+    U = metrics.noisy(Y_mean)
+
+    np.testing.assert_allclose(Y_mean, U, atol=4*sd)
         
-        U = metrics.ucb(Y_mean, Y_var, beta=beta)
-        np.testing.assert_equal(U, Y_mean)
+def test_ucb(Y_mean, Y_var, beta):
+    U = metrics.ucb(Y_mean, Y_var, beta)
 
-    def test_ucb_with_var(self):
-        Y_mean = np.arange(10)
-        Y_var = np.ones(Y_mean.shape)
-        beta = 2
-        
-        U = metrics.ucb(Y_mean, Y_var, beta=beta)
-        np.testing.assert_allclose(U, Y_mean+beta)
+    np.testing.assert_allclose(U, Y_mean + beta*np.sqrt(Y_var))
 
-    def test_ts_no_var(self):
-        Y_mean = np.arange(10)
+def test_ts_no_var(Y_mean):
+    Y_var = np.zeros(Y_mean.shape)
+    U = metrics.thompson(Y_mean, Y_var)
+    np.testing.assert_allclose(U, Y_mean)
 
-        Y_var = np.zeros(Y_mean.shape)
-        U = metrics.thompson(Y_mean, Y_var)
-        np.testing.assert_allclose(U, Y_mean)
+def test_ts_tiny_var(Y_mean):
+    Y_var = np.full(Y_mean.shape, 0.01)
+    U = metrics.thompson(Y_mean, Y_var)
+    np.testing.assert_allclose(U, Y_mean, atol=0.4)
 
-    def test_ts_tiny_var(self):
-        Y_mean = np.arange(10)
+def test_thompson(Y_mean, Y_var, stochastic):
+    Y_var = np.zeros(Y_mean.shape)
+    U = metrics.thompson(Y_mean, Y_var, stochastic)
 
-        Y_var = np.full(Y_mean.shape, 0.01)
-        U = metrics.thompson(Y_mean, Y_var)
+    if stochastic:
+        np.testing.assert_equal(Y_mean, U)
+    else:
         np.testing.assert_allclose(U, Y_mean, atol=0.4)
 
-    def test_ts_stochastic(self):
-        Y_mean = np.arange(10)
+def test_ei(Y_mean: np.ndarray, Y_var_0: np.ndarray, xi, curr_max):
+    U = metrics.ei(Y_mean, Y_var_0, curr_max, xi)
 
-        Y_var = np.zeros(Y_mean.shape)
-        U = metrics.thompson(Y_mean, Y_var, stochastic=True)
-        np.testing.assert_equal(U, Y_mean)
+    np.testing.assert_array_less(0, U[Y_mean + xi > curr_max])
+    np.testing.assert_array_less(U[Y_mean + xi <= curr_max], 0)
 
-    def test_ei_no_imp(self):
-        Y_mean = np.arange(10)
-        Y_var = np.zeros(Y_mean.shape)
-        xi = 0.
-        curr_max = 100
+def test_pi(Y_mean: np.ndarray, Y_var_0: np.ndarray, xi, curr_max):
+    U = metrics.pi(Y_mean, Y_var_0, curr_max, xi)
 
-        U = metrics.ei(Y_mean, Y_var, current_max=curr_max, xi=xi)
-        np.testing.assert_array_less(U, np.zeros(Y_mean.shape))
+    np.testing.assert_array_equal(1, U[Y_mean + xi > curr_max])
+    np.testing.assert_array_equal(0, U[Y_mean + xi <= curr_max])
 
-    def test_ei_with_imp(self):
-        Y_mean = np.arange(10)
-        Y_var = np.zeros(Y_mean.shape)
-        xi = 0.
-        curr_max = 0
+def test_threshold(Y_mean: np.ndarray, threshold: float):
+    U = metrics.threshold(Y_mean, threshold)
 
-        U_2 = metrics.ei(Y_mean, Y_var, current_max=curr_max, xi=xi)
-        np.testing.assert_array_less(np.full(Y_mean.shape, -1.), U_2)
-
-    def test_pi_no_imp(self):
-        Y_mean = np.arange(10)
-        Y_var = np.zeros(Y_mean.shape)
-        xi = 0.
-        curr_max = 100
-
-        U = metrics.pi(Y_mean, Y_var, current_max=curr_max, xi=xi)
-        np.testing.assert_array_equal(U, np.zeros(Y_mean.shape))
-
-    def test_pi_with_imp(self):
-        Y_mean = np.arange(10)
-        Y_var_1 = np.zeros(Y_mean.shape)
-        xi = 0.
-        curr_max = -1.
-
-        U = metrics.pi(Y_mean, Y_var_1, current_max=curr_max, xi=xi)
-        np.testing.assert_array_less(np.zeros(Y_mean.shape), U)
-
-    def test_threshold_all_above(self):
-        Y_mean = np.arange(10)
-        U = metrics.random_threshold(Y_mean, threshold=0)
-        np.testing.assert_array_less(U, np.ones(Y_mean.shape))
-
-    def test_threshold_some_above(self):
-        Y_mean = np.arange(10)
-        U = metrics.random_threshold(Y_mean, threshold=4)
-        self.assertTrue((U[:4] == -1.).all() and (U[4:] >= 0).all)
-
-    def test_threshold_all_below(self):
-        Y_mean = np.arange(10)
-        U = metrics.random_threshold(Y_mean, threshold=10)
-        np.testing.assert_allclose(U, np.full(Y_mean.shape, -1.))
-        
-if __name__ == "__main__":
-    unittest.main()
+    np.testing.assert_array_less(0, U[Y_mean >= threshold])
+    np.testing.assert_array_equal(-1, U[Y_mean < threshold])
