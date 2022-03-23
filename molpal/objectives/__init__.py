54298bfc13862c88604a7318f05ecf5f55fd0c2d
jenna-fromer <jfromer@mit.edu>
2022-03-23 09:36:46 -0400

    uncomment code

--- molpal/objectives/__init__.py
+++ molpal/objectives/__init__.py
@@ -1,24 +1,14 @@
-from typing import Iterable, Type
+from typing import Type
 
 from molpal.objectives.base import Objective
 
-# def objective(
-#     *args, **kwargs
-#     objectives: Iterable[str], objective_configs: Iterable[str],
-#     minimize: Iterable[bool], path: str, verbose: int = 0,
-#     **kwargs
-# ) -> Objective:
-#     return Objective(*args, **kwargs)
-#     """Objective factory function"""
+def objective(objective, objective_config: str, **kwargs) -> Type[Objective]:
+    """Objective factory function"""
+    if objective == 'docking':
+        from molpal.objectives.docking import DockingObjective
+        return DockingObjective(objective_config, **kwargs)
+    if objective == 'lookup':
+        from molpal.objectives.lookup import LookupObjective
+        return LookupObjective(objective_config, **kwargs)
     
-#     for o, config, mini in zip(objectives, objective_configs, minimize):
-#         if o == 'docking':
-#             from molpal.objectives.docking import DockingObjective
-#             return DockingObjective(
-#                 config, minimize=mini, path=path, verbose=verbose
-#             )
-#         if o == 'lookup':
-#             from molpal.objectives.lookup import LookupObjective
-#             return LookupObjective(config, **kwargs)
-    
-#     raise NotImplementedError(f'Unrecognized objective: "{objective}"')
+    raise NotImplementedError(f'Unrecognized objective: "{objective}"')
