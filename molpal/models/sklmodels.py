7fc75549f5e9f5e1859b16c601bf5d7fe88a754e
david graff <deg711@g.harvard.edu>
2022-03-25 11:10:56 -0400

    more formatting

--- molpal/models/sklmodels.py
+++ molpal/models/sklmodels.py
@@ -143,11 +143,9 @@ class GPModel(Model):
     ):
         test_batch_size = test_batch_size or 1024
 
-        kernel = {
-            "dot": kernels.DotProduct,
-            "matern": kernels.Matern,
-            "rbf": kernels.RBF,
-        }[gp_kernel]()
+        kernel = {"dot": kernels.DotProduct, "matern": kernels.Matern, "rbf": kernels.RBF}[
+            gp_kernel
+        ]()
 
         self.model = GaussianProcessRegressor(
             kernel=kernel, normalize_y=True, random_state=model_seed
